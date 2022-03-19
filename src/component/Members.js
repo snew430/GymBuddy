3557d24e6b254e58e30e21b493c494538753fdb5
snew430 <seanpnew@hotmail.com>
2022-03-18 22:10:53 -0400

    added members

--- src/component/Members.js
+++ src/component/Members.js
@@ -1,9 +1,11 @@
+import { FaTimes } from "react-icons/fa";
+
 export default function Members({ member, onDelete }) {
   function handleDeleteClick() {
     onDelete(member.id);
   }
   return (
-    <div>
+    <div style={memberCard}>
       <label>
         <input
           type="checkbox"
@@ -12,6 +14,21 @@ export default function Members({ member, onDelete }) {
         />
         {member.name}
       </label>
+      <FaTimes style={deleteBtn} />
     </div>
   );
 }
+
+const deleteBtn = {
+  color: "red",
+  cursor: "pointer",
+};
+
+const memberCard = {
+  border: "solid 1px",
+  borderRadius: "5px",
+  margin: "10px",
+  padding: "10px",
+  width: "25%",
+  textAlign: "center"
+};
